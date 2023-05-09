cd9dacd5d5409658870ed40299f426fb2670da58
OArcher07 <117929539+OArcher07@users.noreply.github.com>
2023-05-09 10:00:51 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2020 Ania Kubow
+
+const switchElement = document.querySelector('.switch')
+
+// When the switch element is clicked, it will run the function 'dark'
+switchElement.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
